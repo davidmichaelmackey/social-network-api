d93778b40351ccdc0aa3dacb923c432e1324d68f
David Mackey <davidmackey92@gmail.com>
2023-04-10 21:43:47 -0400

    Reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,39 @@
 const { Schema, default: mongoose } = require('mongoose');
 
-// code here //
+const reactionsSchema = new Schema(
+  {
+    reactionId:
+    {
+      type: mongoose.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody:
+    {
+      type: String,
+      required: true,
+      maxLength: [ 280, 'Must be less than 280 characters!' ]
+    },
+    username:
+    {
+      type: String,
+      required: true
+    },
+    createdAt:
+    {
+      type: Date,
+      default: Date.now,
+      get: function(timestamp) {
+        return new Date(timestamp).toLocaleDateString();
+      }
+    }
+  },
+  {
+    toJSON:
+    {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionsSchema;
