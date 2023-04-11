5803275fbd9f1fc6081134a6bd771988aa2de1d0
David Mackey <davidmackey92@gmail.com>
2023-04-10 21:29:01 -0400

    server updates after config/connection.js

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.json());
+app.use(routes);
 
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once('open', () => {
+  app.listen(PORT, () => { console.log(`Connected to the server at: http://localhost:${PORT}`); });
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-
-
-app.listen(PORT, () => console.log(`🌍 Connected on http://localhost:${PORT}/`));
